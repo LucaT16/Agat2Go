f889bd089a307056ab72507f7745097f040681d4
LucaT16 <lucatallarita@googlemail.com>
2019-09-26 17:41:42 +0200

    Update config

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -16,7 +16,7 @@ export class FirebaseService {
     public afAuth: AngularFireAuth
   ){}
 
-  getTasks(){
+  getProducts(){
     return new Promise<any>((resolve, reject) => {
       this.afAuth.user.subscribe(currentUser => {
         if(currentUser){
